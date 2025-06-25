4b7029f1d9bddcee59e9cd634f09627218db2b75
lanceli <lanceli1001x@hotmail.com>
2025-06-25 15:10:37 +0800

    Refactoring:use git repo

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,8 +9,6 @@ buildscript {
         }
         mavenCentral()
 
-        // This is used only for internal Google builds.
-        maven { url = uri("../nowinandroid-prebuilts/m2repository") }
     }
     dependencies {
         classpath(libs.google.oss.licenses.plugin) {
@@ -28,15 +26,16 @@ plugins {
     alias(libs.plugins.compose) apply false
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.kotlin.serialization) apply false
-    alias(libs.plugins.dependencyGuard) apply false
+//    alias(libs.plugins.dependencyGuard) apply false
     alias(libs.plugins.firebase.crashlytics) apply false
     alias(libs.plugins.firebase.perf) apply false
     alias(libs.plugins.gms) apply false
-    alias(libs.plugins.hilt) apply false
+//    alias(libs.plugins.hilt) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.roborazzi) apply false
     alias(libs.plugins.secrets) apply false
     alias(libs.plugins.room) apply false
     alias(libs.plugins.openapi.generator) apply false
     alias(libs.plugins.module.graph) apply true // Plugin applied to allow module graph generation
+    alias(libs.plugins.spring.boot) apply false
 }
